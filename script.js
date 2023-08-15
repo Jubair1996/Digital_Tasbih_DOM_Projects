81596d2d79d76898a39c3ae40b9aeb570e8d6961
Jubari1996 <abujubari1996@gmail.com>
2023-08-15 15:26:41 +0600

    Subhanallah js completed

--- script.js
+++ script.js
@@ -45,3 +45,48 @@ subhanAllahDecrimentBtn.addEventListener("click", function () {
   subhanAllahInitialValue -= 1;
   subhanAllahDisplay.innerText = subhanAllahInitialValue;
 });
+
+alhamdulillahIncrimentBtn.addEventListener("click", function () {
+  if (alhamdulillahInitialValue === 33) {
+    return alert("Alhamdulillah Complete. Please Fillup anoter one");
+  }
+
+  alhamdulillahInitialValue += 1;
+  alhamdulillahDisplay.innerText = alhamdulillahInitialValue;
+});
+
+alhamdulillahDecrimentBtn.addEventListener("click", function () {
+  if (alhamdulillahInitialValue === 0) {
+    return alert("You can't added nagetive value");
+  }
+
+  alhamdulillahInitialValue -= 1;
+  alhamdulillahDisplay.innerText = alhamdulillahInitialValue;
+});
+
+allahAkberIncrimentBtn.addEventListener("click", function () {
+  if (allahAKberInitialValue === 34) {
+    return alert("Allahu akber Complete. Please Fillup anoter one");
+  }
+
+  allahAKberInitialValue += 1;
+  allahAkberDisplay.innerText = allahAKberInitialValue;
+});
+
+allahAkberDecrimentBtn.addEventListener("click", function () {
+  if (allahAKberInitialValue === 0) {
+    return alert("You can't added nagetive value");
+  }
+
+  allahAKberInitialValue -= 1;
+  allahAkberDisplay.innerText = allahAKberInitialValue;
+});
+
+resetBtn.addEventListener("click", function () {
+  subhanAllahDisplay.innerText = 0;
+  alhamdulillahDisplay.innerText = 0;
+  allahAkberDisplay.innerText = 0;
+  subhanAllahInitialValue = 0;
+  alhamdulillahInitialValue = 0;
+  allahAKberInitialValue = 0;
+});
