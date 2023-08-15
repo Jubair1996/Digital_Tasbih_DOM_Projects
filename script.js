c6eb005149cedf1eb1976c53024b277a59cb11d2
Jubari1996 <abujubari1996@gmail.com>
2023-08-15 15:18:23 +0600

    Subhanallah js completed

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
-const subhanAllahDisplay = document.getElementById('subhanAllahDisplay')
-const subhanAllahIncrimentBtn = document.getElementById('subhanAllahIncrimentBtn')
-const subhanAllahDecrimentBtn = document.getElementById('subhanAllahDecrimentBtn')
+const subhanAllahDisplay = document.getElementById("subhanAllahDisplay");
+const subhanAllahIncrimentBtn = document.getElementById(
+  "subhanAllahIncrimentBtn"
+);
+const subhanAllahDecrimentBtn = document.getElementById(
+  "subhanAllahDecrimentBtn"
+);
 
-const alhamdulillahDisplay = document.getElementById('alhamdulillahDisplay')
-const alhamdulillahIncrimentBtn = document.getElementById('alhamdulillahIncrimentBtn')
-const alhamdulillahDecrimentBtn = document.getElementById('alhamdulillahDecrimentBtn')
+const alhamdulillahDisplay = document.getElementById("alhamdulillahDisplay");
+const alhamdulillahIncrimentBtn = document.getElementById(
+  "alhamdulillahIncrimentBtn"
+);
+const alhamdulillahDecrimentBtn = document.getElementById(
+  "alhamdulillahDecrimentBtn"
+);
 
-const allahAkberDisplay = document.getElementById('allahAkberDisplay')
-const allahAkberIncrimentBtn = document.getElementById('allahAkberIncrimentBtn')
-const allahAkberDecrimentBtn = document.getElementById('allahAkberDecrimentBtn')
+const allahAkberDisplay = document.getElementById("allahAkberDisplay");
+const allahAkberIncrimentBtn = document.getElementById(
+  "allahAkberIncrimentBtn"
+);
+const allahAkberDecrimentBtn = document.getElementById(
+  "allahAkberDecrimentBtn"
+);
 
-const resetBtn = document.getElementById('resetBtn')
+const resetBtn = document.getElementById("resetBtn");
+
+let subhanAllahInitialValue = 0;
+let alhamdulillahInitialValue = 0;
+let allahAKberInitialValue = 0;
+
+subhanAllahIncrimentBtn.addEventListener("click", function () {
+  if (subhanAllahInitialValue === 33) {
+    return alert("Subhan Allah Complete. Please Fillup anoter one");
+  }
+
+  subhanAllahInitialValue += 1;
+  subhanAllahDisplay.innerText = subhanAllahInitialValue;
+});
+
+subhanAllahDecrimentBtn.addEventListener("click", function () {
+  if (subhanAllahInitialValue === 0) {
+    return alert("You can't added nagetive value");
+  }
+
+  subhanAllahInitialValue -= 1;
+  subhanAllahDisplay.innerText = subhanAllahInitialValue;
+});
